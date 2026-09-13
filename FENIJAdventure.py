68f5d1e7e36c1107afc02313858f17637fe4c419
Officer Ferret <194603164+officer-ferret@users.noreply.github.com>
2026-09-13 20:31:26 +0200

    Funktionen fürs speichern des Spiels, Pythondatei für Funktionen zum auslagern, .gitignore um nutzlose Ordner nicht hochzuladen

--- FENIJAdventure.py
+++ FENIJAdventure.py
@@ -1,47 +1,12 @@
+from funktionen import lade_spiel, speichere_spiel, neuer_charakter
 
-Charakteranzahl = int(input("Mit wievielen Charakteren willst du Spielen? 1-5: "))
+spiel_daten = lade_spiel()
+charakter_liste = spiel_daten["charaktere"]
 
-
-def neuer_charakter():
-
-    name = input("Gib deinem Charakter einen Namen: ")
-
-    alter = int(input("Gib deinem Charakter ein Alter: "))
-
-    klasse = input(
-        "Gib deinem Charakter eine Klasse, wähle einen Klassenpfad aus: "
-        "Magier, Krieger, Priester, Schurke: "
-    )
-
-    SchlechteEigenschaft = input(
-        "Gib deinem Charakter eine manchmal schädliche persönliche Eigenschaft: "
-        "Faul, Zornig, Hyperaktiv, Gierig, Besserwisser: "
-    )
-
-    GuteEigenschaft = input(
-        "Gib deinem Charakter eine persönliche nützliche Eigenschaft: "
-        "Perfektionist, Ehrgeizig, Intelligent, Stark, Goldene Zunge: "
-    )
-
-    if (
-        klasse in ["Magier", "Krieger", "Priester", "Schurke"]
-        and SchlechteEigenschaft in ["Faul", "Zornig", "Hyperaktiv", "Gierig", "Besserwisser"]
-        and GuteEigenschaft in ["Perfektionist", "Ehrgeizig", "Intelligent", "Stark", "Goldene Zunge"]
-    ):
-        return {
-            "name": name,
-            "alter": alter,
-            "klasse": klasse,
-            "s.Eigenschaft": SchlechteEigenschaft,
-            "g.Eigenschaft": GuteEigenschaft
-        }
-
-    else:
-        print("Charaktererstellung ist fehlgeschlagen, versuche es nochmal")
-
-
-charakter_liste = []
-
-charakter_liste.append(neuer_charakter())
+# Wenn es keine Charaktere gibt, soll einer hinzugefügt werden
+if len(charakter_liste) == 0:
+    Charakteranzahl = int(input("Mit wievielen Charakteren willst du Spielen? 1-5: "))
+    charakter_liste.append(neuer_charakter())
+    speichere_spiel(spiel_daten)
 
 print(charakter_liste)
